2957c333d64a8637fc8028df6ee492682bbaf25e
Vanaraj C <vanarajcs@gmail.com>
2018-03-03 19:41:23 +0530

    Deleted unwanted contents

--- src/app/layouts/default/layout-default.module.ts
+++ src/app/layouts/default/layout-default.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LayoutsDefaultContentComponent } from './content/layouts-default-content.component';
-import { LayoutsDefaultHeaderComponent } from './header/layouts-default-header.component';
-import { LayoutsDefaultFooterComponent } from './footer/layouts-default-footer.component';
 import { LayoutDefaultComponent } from "./layout-default.component";
 
 @NgModule({
@@ -14,17 +11,11 @@ import { LayoutDefaultComponent } from "./layout-default.component";
       NgbModule.forRoot()
   ],
   declarations: [
-      LayoutsDefaultContentComponent,
-      LayoutsDefaultHeaderComponent,
-      LayoutsDefaultFooterComponent,
       LayoutDefaultComponent
   ],
   providers: [],
   exports : [
       LayoutDefaultComponent,
-      LayoutsDefaultContentComponent,
-      LayoutsDefaultHeaderComponent,
-      LayoutsDefaultFooterComponent
   ]
 })
 export class LayoutDefaultModule { }
